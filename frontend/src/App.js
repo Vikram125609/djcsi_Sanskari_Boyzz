7b1838f877c13c8d0e5e73a091d62ea7cc62eccd
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-04-08 17:44:22 +0530

    enroll added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import Enroll from "./components/Enroll";
-import ImageUpload from "./components/ImageUpload";
 import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-3xl text font-sans">hello world!</p>
-    </div>
+    <Enroll/>
   );
 }
 
--- frontend/src/components/Enroll.jsx
+++ frontend/src/components/Enroll.jsx
@@ -1,20 +1,75 @@
-import React from 'react'
-import ImageUpload from './ImageUpload';
+import React from "react";
+import ImageUpload from "./ImageUpload";
+import "../styles/upload.css";
 
 function Enroll() {
   return (
-    <div className='flex content-center'>
-        <div className="image_upload flex justify-center bg-black">
-            <ImageUpload/>
+    <div className="enroll_container bg-[#f8f8fb]  my-20  rounded-lg p-12 drop-shadow-lg h-fit w-[40%] mx-auto">
+      <p className="text-center text-4xl mb-12 font-[cambria] font-semibold	">
+        Enroll Employees
+      </p>
+      <div className="flex content-center flex-col h-fit justify-center">
+        <div className="">
+          <label
+            for="4"
+            className="block w-full pb-1 text-sm  transition-all duration-200 ease-in-out group-focus-within:text-blue-[400] my-[10px] font-bold"
+          >
+            Name :
+          </label>
+          <input
+            placeholder="Enter Your full name"
+            id="4"
+            type="text"
+            className="peer h-10 w-full rounded-md bg-gray-50 px-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:shadow-lg focus:shadow-blue-[400] border-2"
+          />
         </div>
-        <div className="aadhar_details_container flex">
-            image upload
+        <div className="">
+          <label
+            for="4"
+            className="block w-full pb-1 text-sm  transition-all duration-200 ease-in-out group-focus-within:text-blue-[400] my-[10px] font-bold"
+          >
+            Select Your Gender :
+          </label>
+          <div class="container h-[65%] flex align-middle justify-center content-center">
+            <div class="select-wrapper w-full">
+              <select class="select  peer h-10 w-full rounded-md bg-gray-50 px-8  font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:shadow-lg focus:shadow-blue-[400] border-2">
+                <option value="value1">Gender</option>
+                <option value="value1">Male</option>
+                <option value="value2">Female</option>
+                <option value="value3">Other</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div className="pan_details_container flex">
-            image upload
+        <div class=" ">
+          <p className="flex my-[10px] font-bold">Aaddhar card image :</p>
+          <div className=" image_upload flex  h-fit ">
+            <ImageUpload />
+          </div>
         </div>
+        <div class=" h-fit">
+          <p className="flex my-[10px] font-bold">Pan card image :</p>
+          <div className=" image_upload flex h-fit ">
+            <ImageUpload />
+          </div>
+        </div>
+
+        <div class="flex mb-4 h-fit ">
+          <div class=" ">
+            <p className="flex my-[10px] font-bold">Person's Facial Image :</p>
+            <div className=" image_upload flex  h-fit ">
+              <ImageUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button class="rounded-lg px-8 py-2 text-xl border-2  text-black-500 hover:bg-[#cba211] hover:shadow-2xl hover:text-white duration-300">
+          Verify
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Enroll;
