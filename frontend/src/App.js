b5d62a3b3b4710447fdaaf93be65ede62315b7f1
iamparthvaghela <parthvgla@gmail.com>
2023-04-08 12:43:19 +0530

    snakeshwar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import React from 'react';
 import { messaging } from "./firebase";
 import { getToken } from "firebase/messaging";
 function App() {
@@ -9,6 +10,7 @@ function App() {
       const token = await getToken(messaging, {
         vapidKey: "BOcK_mXF0Alh6rclU6KZFHkqTnUWGn_YrPMAyLIfC2KULgqJedUxD-_ZiYFNUd4LsB5P9poiJ2AOpEyMqLTJE50"
       })
+      console.log(token)
       localStorage.setItem('FCM', token)
     }
   }
